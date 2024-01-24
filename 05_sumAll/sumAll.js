a91e93a693ae70caef3a2d14fa8740a8073ff93b
CamilleOnoda <info@camilleonoda.com>
2024-01-24 15:34:40 +0900

    Include alternative to check is arg is of type Integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
+//if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2))
     if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
         return 'ERROR';
     } else {
