a0d95c5ad3197487ba86da708b789ad07423387b
CamilleOnoda <info@camilleonoda.com>
2024-01-24 14:37:26 +0900

    Wrtie a function that takes 2 integers and returns the sum of every number between(and including) them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } if (num1.isNan() || num2.isNan()) {
+        return 'ERROR';
+    } else {
+        let maxNum = Math.max(num1, num2);
+        let minNum = Math.min(num1, num2);
 
+        for (let i = minNum; i <= maxNum; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
