767eb67fa46c0c439c345603b74fa0bf59fe11b7
CamilleOnoda <info@camilleonoda.com>
2024-01-24 14:35:59 +0900

    Write a function that takes an array and any nb of args and delete any occurence of args from the array and return it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let str of args) {
+        const index = array.indexOf(str);
+        if (index !== -1) {
+            array.splice(index,1);}
+        }
+    return array;
 };
 
 // Do not edit below this line
