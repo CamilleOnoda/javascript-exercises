b90f9f023d95c909c1291ff3a34a8eb7c581bae5
CamilleOnoda <info@camilleonoda.com>
2024-01-25 12:34:45 +0900

    Write functions to convert celcius to fahrenheit and vice versa and round the results to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const FtoC = (fahrenheit - 32) / (9 / 5);
+  const celciusRounded = Math.round(FtoC * 10) / 10;
+  return celciusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const CtoF = celcius * (9 /5) + 32;
+  const fahrenheitRounded = Math.round(CtoF * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
